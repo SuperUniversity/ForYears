eebcaf5c5216eb63d1d24c7cd70c3ab804b79d1e
GingiWang <pokitimmy@hotmail.com>
2017-06-18 13:28:42 +0800

    123

---
+++ FourYears/Areas/FoodMapArea/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using FourYears.Areas.FoodMapArea.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FourYears.Areas.FoodMapArea.Controllers
+{
+    public class AccountController : Controller
+    {
+        private superuniversityEntities db = new superuniversityEntities();
+        // GET: FoodMapArea/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(ShopCustomer shopcustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShopCustomer.Add(shopcustomer);
+                db.SaveChanges();
+                return RedirectToAction("Login", "Account", new { Area = "FoodMapArea" });
+            }
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(ShopCustomer shopcustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginUser = db.ShopCustomer.FirstOrDefault(u => u.EmailAddress == shopcustomer.EmailAddress && u.Password == shopcustomer.Password);
+                if (loginUser != null)
+                {
+                    Response.Cookies["UserName"].Value = loginUser.FullName;
+                    Response.Cookies["CustomerID"].Value = loginUser.CustomerID.ToString();
+                    return RedirectToAction("Index", "Customer", new { Area = "FoodMapArea" });
+                }
+            }
+
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Response.Cookies["UserName"].Expires = DateTime.Now.AddSeconds(-1);
+            Response.Cookies["CustomerID"].Expires = DateTime.Now.AddSeconds(-1);
+            Session.Abandon();
+            return RedirectToAction("Index", "Customer", new { Area = "FoodMapArea" });
+        }
+    }
+}
---
+++ FourYears/Areas/FoodMapArea/Models/Partial/ShopCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FourYears.Areas.FoodMapArea.Models
+{
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class ShopCustomer
+    {
+        public class CustomerMetadata
+        {
+            [DisplayName("會員編號")]
+            public int CustomerID { get; set; }
+            [DisplayName("會員姓名")]
+            [Required(ErrorMessage = "請輸入您的姓名")]
+            public string FullName { get; set; }
+            [DisplayName("會員帳號")]
+            [DataType(DataType.EmailAddress)]
+            [EmailAddress(ErrorMessage = "電子信箱格式錯誤")]
+            [Required(ErrorMessage = "請輸入電子信箱")]
+            public string EmailAddress { get; set; }
+            [DisplayName("會員密碼")]
+            [DataType(DataType.Password)]
+            [Required(ErrorMessage = "請輸入密碼")]
+            public string Password { get; set; }
+        }
+    }
+}
