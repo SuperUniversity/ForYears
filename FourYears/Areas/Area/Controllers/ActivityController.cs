a8273d302e5f62134cdd4c9dcd496bea84ca621d
jeremy chen <aiongameop@gmail.com>
2017-06-21 13:51:06 +0800

    123444

---
+++ FourYears/Areas/Area/Controllers/ActivityController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FourYears.Areas.Area.Models;
+
+namespace FourYears.Areas.Area.Controllers
+{
+    public class ActivityController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            List<Models.Activity> Activites = ActivityDataContext.LoadActivites();
+            return View(Activites);
+        }
+        public ActionResult Insert()
+        {
+            if (Request.Form.Count > 0)
+            {
+                Models.Activity _Activity = new Models.Activity();
+                _Activity.ActivityName = Request.Form["ActivityName"];
+                _Activity.Description = Request.Form["Description"];
+                _Activity.Location = Request.Form["Location"];
+                _Activity.Host = Request.Form["Host"];
+                ActivityDataContext.InsertActivity(_Activity);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Models.Activity _Activity = ActivityDataContext.LoadActivityByID(id);
+            return View(_Activity);
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            Models.Activity _Activity = new Models.Activity();
+            _Activity.ActivityID = Convert.ToInt32(Request.Form["ActivityID"]);
+            _Activity.ActivityName = Request.Form["ActivityName"];
+            _Activity.Description = Request.Form["Description"];
+            _Activity.Location = Request.Form["Location"];
+            _Activity.Host = Request.Form["Host"];
+            ActivityDataContext.EditActivity(_Activity);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id = 0)
+        {
+
+            ActivityDataContext.DeleteActivity(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
---
+++ FourYears/Areas/Area/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FourYears.Areas.Area.Models
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public string ActivityName { get; set; }
+        public int ClassID { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public string Host { get; set; }
+    }
+}
